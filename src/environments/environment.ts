bd810920f1a595c24089998375bc9a1130778cff
danirirom3-hub <danirirom30@gmail.com>
2026-05-03 22:33:44 -0500

    se organizaron comentarios en demas componentes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,6 @@ export const environment = {
   // Indica que este es el entorno de desarrollo
   production: false,
 
-  // URL del backend en local (Spring Boot, Node, etc.)
-  // OJO: no es el puerto de Angular (4200), es el del backend
+  // URL del back
   apiUrl: 'http://localhost:8080/api'
 };
